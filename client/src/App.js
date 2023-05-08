f65b952c7f674cc81cfecc5b7c1388d4f2781c34
phanquangduong2002 <phanquangduong2002@gmail.com>
2023-05-08 17:37:09 +0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">Beauty Store</div>;
 }
 
 export default App;
